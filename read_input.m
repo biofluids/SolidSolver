1ab819238d4c049351af112e93c8f901a7640317
jiecheng <chengjiehust@gmail.com>
2015-06-07 13:01:01 -0400

    reading abaqus input file/ongoing

--- read_input.m
+++ read_input.m
@@ -1,76 +1,25 @@
-function [materialprops,nsd,gravity,ned,nn,coords,nel,nen,connect,ng,gnodes,nh,hnodes] = ...
-    read_input(infile)
-cellarray=textscan(infile,'%s');
-cellno = 0;
-materialprops = zeros(5,1);
-for i=1:5
-    cellno=cellno+2;
-    materialprops(i)=str2num(cellarray{1}{cellno});
+function [nn,coords,nel,connect]=read_input(nsd,nen,infile)
+content=fgets(infile);
+while(strncmp('*Node',content,5)==0)
+    content=fgets(infile);
 end
-cellno = cellno + 2;
-nsd = str2num(cellarray{1}{cellno});
-cellno = cellno + 1;
-gravity = zeros(nsd,1);
-for i=1:nsd
-    cellno=cellno+1;
-    gravity(i)=str2num(cellarray{1}{cellno});
-end
-cellno = cellno + 2;
-ned = str2num(cellarray{1}{cellno});
-cellno = cellno + 2;
-nn = str2num(cellarray{1}{cellno});
-cellno = cellno + 1;
+content=fgets(infile);
 % coords
-coords = zeros(nsd,nn);
-for i = 1 : nn
-     for j = 1 : nsd
-       cellno = cellno + 1;
-       coords(j,i) = str2num(cellarray{1}{cellno});
-     end
+while(strncmp('*',content,1)==0)
+    temp=str2num(content);
+    nn=temp(1);
+    coords(:,nn)=transpose(temp(2:(nsd+1)));
+    content=fgets(infile);
 end
-cellno = cellno + 2;
-nel = str2num(cellarray{1}{cellno});
-cellno = cellno + 2;
-nen = str2num(cellarray{1}{cellno});
-cellno = cellno + 1;
 % connect
-connect = zeros(nel,nen);
-for i = 1:nel
-    for j = 1:nen
-        cellno = cellno + 1;
-        connect(i,j) = str2num(cellarray{1}{cellno});
-    end
+while(strncmp('*Element',content,8)==0)
+    content=fgets(infile);
 end
-connect = transpose(connect);
-cellno = cellno + 2;
-ng = str2num(cellarray{1}{cellno});
-cellno = cellno + 3;
-% gnodes
-gnodes = zeros(ng,3);
-for i = 1:ng
-    for j = 1:3
-        cellno = cellno + 1;
-        gnodes(i,j) = str2num(cellarray{1}{cellno});
-    end
+content=fgets(infile);
+while(strncmp('*',content,1)==0)
+    temp=str2num(content);
+    nel=temp(1);
+    connect(:,nel)=transpose(temp(2:(1+nen)));
+    content=fgets(infile);
 end
-gnodes = transpose(gnodes);
-cellno = cellno + 2;
-nh = str2num(cellarray{1}{cellno});
-% hnodes
-cellno = cellno + 3;
-hnodes = zeros(nh,4);
-for i = 1:nh
-    for j = 1:4
-        cellno = cellno + 1;
-        hnodes(i,j) = str2num(cellarray{1}{cellno});
-    end
 end
-hnodes = transpose(hnodes);
-end
-
-
-
-
-
-
-
